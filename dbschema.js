ca3b2cec2a62003fd0f4a1c7575d9eb014ef9458
Griffin McCauley <griffin_mccauley@brown.edu>
2021-06-28 13:59:09 -0600

    added some dbschema notes

--- dbschema.js
+++ dbschema.js
@@ -10,6 +10,7 @@ let db = {
       class: "2022",
       concentrations: ["Applied Mathematics","",""], //come from drop down bar
       preferredPronouns: "he/him", //another drop down to avoid trolling (optional)
+      // add profileImages
       // character info
       interests: ["Sports Statistics","Nutrition","Movie Cataloging","Data Science","Music"], // 3 to 5
       groups: ["The Starting Eleven","",""], // clubs and affinity groups
@@ -21,7 +22,7 @@ let db = {
         book: "Animal Farm",
         tvShow: "How I Met Your Mother",
         artist: "Queen",
-      }, // 3 to 4? 
+      }, // 3 to 4? we might want to make this optional... even though we find it appealing, it might not be the most relevant
       bio: "", // paragraph text field for open response
       courses: [{courseCode:"DATA 2040", courseName:"Deep Learning", color:""},
         {courseCode:"APMA 1740", courseName:"Recent Applications of Probability and Statistics", color:""},
