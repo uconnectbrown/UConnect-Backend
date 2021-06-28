2ad362572baced65b15a5ae9aadd27e76f8dbac6
ethan21814 <ethan_huang1@brown.edu>
2021-06-28 12:15:21 -0400

    2: get all profiles and create new profile

---
+++ dbschema.js
@@ -0,0 +1,9 @@
+let db = {
+  profiles: [
+    {
+      userHandle: "ethan21814",
+      email: "ethan_huang1",
+      createdAt: "2021-06-28T16:11:21.257Z",
+    },
+  ],
+};
--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,55 @@
-// imports and set up
+// Setup
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+
 admin.initializeApp();
+
+const express = require("express");
+const app = express();
 
 // Cloud Functions
 
-// get all Profiles
-exports.getProfiles = functions.https.onRequest((req, res) => {
+// get all profiles
+app.get("/profiles", (req, res) => {
   admin
     .firestore()
     .collection("profiles")
+    .orderBy("createdAt", "desc")
     .get()
     .then((data) => {
       let profiles = [];
       data.forEach((doc) => {
-        profiles.push(doc.data());
+        profiles.push({
+          profileId: doc.id,
+          userHandle: doc.data().userHandle,
+          email: doc.data().email,
+          createdAt: doc.data().createdAt,
+        });
       });
       return res.json(profiles);
     })
     .catch((err) => console.error(err));
 });
+
+// create new profile
+app.post("/profile", (req, res) => {
+  const newProfile = {
+    userHandle: req.body.userHandle,
+    email: req.body.email,
+    createdAt: new Date().toISOString(),
+  };
+
+  admin
+    .firestore()
+    .collection("profiles")
+    .add(newProfile)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created succesfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
+
+exports.api = functions.region("us-east1").https.onRequest(app);
