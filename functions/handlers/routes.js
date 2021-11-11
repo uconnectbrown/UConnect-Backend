0806e4ec808ee58a65f5e28ff11c2e5cac427934
ethan21814 <ethan_huang1@brown.edu>
2021-11-11 18:02:09 -0500

    Minor change

--- functions/handlers/routes.js
+++ functions/handlers/routes.js
@@ -99,7 +99,7 @@ exports.draftFeatured = (req, res) => {
     });
 };
 
-// Draft featured profiles
+// get all emails that currently having a pending request
 exports.getNotifications = (req, res) => {
   let emailIds = [];
   let promises = [];
@@ -899,71 +899,6 @@ exports.editUserDetails = (req, res) => {
       return res.status(500).json({ error: err.code });
     });
 };
-
-// // Create event
-// exports.createEvent = (req, res) => {
-//   let eventId = uuid();
-//   db.collection("events")
-//     .doc(`${eventId}`)
-//     .set({
-//       name: req.body.name,
-//       date: req.body.date,
-//       location: req.body.location,
-//       postedBy: req.body.postedBy,
-//       hostedBy: req.body.hostedBy,
-//     })
-//     .then(() => {
-//       return res.json({ messages: "Event created succesfully" });
-//     })
-//     .catch((err) => res.json({ error: err.code }));
-// };
-
-// // Delete event
-// exports.deleteEvent = (req, res) => {
-//   let eventId = req.params.eventId;
-//   db.collection("events")
-//     .doc(`${eventId}`)
-//     .delete()
-//     .then(() => {
-//       return res.json({ messages: "Event deleted succesfully" });
-//     })
-//     .catch((err) => res.json({ error: err.code }));
-// };
-
-// // Get events
-// exports.getEvents = (res) => {
-//   db.collection("events")
-//     .get()
-//     .then((data) => {
-//       let events = [];
-//       data.forEach((doc) => {
-//         events.push(doc.data());
-//       });
-//       return res.json(events);
-//     })
-//     .catch((err) => res.json({ error: err.code }));
-// };
-
-// // Support event
-// exports.supportEvent = (req, res) => {
-//   let eventId = req.params.eventId;
-//   let supporterId = req.params.supporterId;
-//   db.collection("events")
-//     .doc(`${eventId}`)
-//     .collection("fans")
-//     .doc(`${supporterId}`)
-//     .set({
-//       sent: new Date().toISOString(),
-//       emailId: supproterId,
-//       name: req.body.name,
-//       imageUrl: req.body.imageUrl,
-//       classYear: req.body.classYear,
-//     })
-//     .then(() => {
-//       return res.json({ messages: "Event supported succesfully" });
-//     })
-//     .catch((err) => res.json({ error: err.code }));
-// };
 
 // Get all students in given course
 exports.getStudents = (req, res) => {
